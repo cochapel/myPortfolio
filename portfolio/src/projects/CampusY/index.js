7e8428b61a857de42cfed19cc99e005349a6c47d
Austin Phillips <cochapel@live.unc.edu>
2019-08-26 18:10:11 -0400

    improved work

--- portfolio/src/projects/CampusY/index.js
+++ portfolio/src/projects/CampusY/index.js
@@ -56,6 +56,11 @@ class CampusY extends Component {
               Visual Design
             </p>
 
+            <h3>Timeframe:</h3>
+            <p>
+              3 weeks
+            </p>
+
             <h3>Tools:</h3>
             <p className="margSpace">
               Adobe XD, Adobe Illustrator, Adobe Photoshop, Pen and Paper
@@ -66,27 +71,27 @@ class CampusY extends Component {
           <h2>About the Project</h2>
           <h3>Overview</h3>
           <p>
-            A design proposal for the Campus Y website to help members in the
-            organization make reservations for meetings at the Campus Y
-            building. The design proposal also focuses on guiding students at
-            the University of North Carolina at Chapel Hill to find information
-            related to the Campus Y or other organizations that work alongside
-            them. There was frustration expressed by the co-presidents of the
-            Campus Y for making reservations and navigating within their website
-            to find this information.
+            A design proposal for the Campus Y website that helps members of the
+            organization find the information they are looking for. There was
+            frustration expressed by the communications director and the
+            co-presidents of the Campus Y for making reservations and navigating
+            through their website. I volunteered to help make this a better
+            experience for the members of the organization and students by using
+            a human-centered design approach to their website.
           </p>
 
           <h3>Solution</h3>
           <p>
-            To restructure the main navigation of the site to better match
-            subcategories beneath them, and to create a more understanding
-            design in order to help navigate users where they intend to go.
+            To restructure the main navigation of the site to better match the
+            subcategories associated with them, and to create a more
+            understanding user interface to help guide users where they intend
+            to go.
           </p>
 
           <h3>Problem Statement</h3>
           <p>
             How might we improve the navigation for the Campus Y website and
-            improve the design in order to attract a larger audience?
+            make improvements on the user flow?
           </p>
 
           <div className="center">
@@ -97,60 +102,61 @@ class CampusY extends Component {
             <p className="caption">**Final homepage design</p>
           </div>
 
-          {/** Background context about the project */}
-          <h2>Introduction</h2>
           <h3>Background Context</h3>
           <p>
-            The Campus Y is a student organization meant to help engage
-            students in social justice around UNC-Chapel Hill. They are often
-            involved in activities that help promote equality around campus
-            and to help influence students to speak up in order to promote
-            social change. The purpose for their website is to guide students
-            in a direction to follow an organization that relates to their
-            passions.
-          </p>
-
-          <h3>My Mission</h3>
-          <p>
-            In the redesign challenge my focus was on two audiences, the
-            Campus Y members and the students at UNC-Chapel Hill. Based on
-            usability tests, the issues with the current site are primarily to
-            do with the layout and the site architecture. I used card sorting
-            to help formulate a more efficient user flow. This method was
-            helpful in guiding the information architecture in a way that is
-            most convenient and easy to use. Interestingly, not many users
-            seemed to have trouble finding the reservation page for reserving a
-            room in the Campus Y building, but they showed frustration in the
-            booking process due to the number of steps needed to take. There
-            were many issues for students finding the committees page for the
-            organizations that work alongside the Campus Y as well. These two
-            issues became the main focus for my redesign challenge.
+            The Campus Y is a student organization meant to help engage students
+            in social justice around UNC-Chapel Hill. They are often involved in
+            activities that help promote equality around campus and influence
+            students to speak up for social change. The purpose for their
+            website is to guide students in a direction by offering them
+            opportunities to follow an organization that relates to their
+            values.
           </p>
 
           {/** Running user testing on the old website */}
           <h2>User Testing and Analysis</h2>
+          <h3>My Mission</h3>
           <p>
-            To begin my research, I created an audience analysis chart while
-            talking to the co-presidents of the organization to get an idea of
-            their target range. Afterwards, I began conducting usability tests
-            of the old site in order to keep track of how the website is being
-            used. This helped in creating two personas representing the
-            audience that is most likely to use the site. You can see the
-            audience analysis, usability brief and other methods used in
-            conducting the research below.
+            In the redesign challenge my focus was on two audiences, the Campus
+            Y members and the students at UNC-Chapel Hill. Based on usability
+            tests, the issues with the current website are primarily to do with
+            content organization and the site architecture. I used card sorting
+            to help formulate a more efficient user flow. Interestingly, none
+            of my participants had trouble finding the room reservations page
+            for booking a group meeting at the Campus Y, but they expressed
+            frustration due to the number of steps it takes to complete the
+            room reservation form.
           </p>
 
-          <div className="center">
-            <img src={require("../../images/persona1.png")}
-              alt="First Persona"
-              className="projectImg2"
-            />
+          <p>
+            Another issue that I discovered was the difficulty for students to
+            find certain pages, because the subcategories of the main menu were
+            in an irrelevant location. One of these pages that were difficult to
+            find was the committees page, which is an important page for
+            students to find according to the communications director and the
+            co-presidents.  Even if a student managed to find the committees
+            page, they struggled to find a particular group they were interested
+            in because of how the content was organized. Therefore, I made the
+            site architecture, room reservations page, and the group committees
+            page the main focus for this website redesign.
+          </p>
 
-            <img src={require("../../images/persona2.png")}
-              alt="Second Persona"
-              className="projectImg2"
-            />
-          </div>
+          <h3>Audience Analysis and Usability Tests</h3>
+          <p>
+            I began this project by creating an audience analysis chart while
+            talking to the communications director and the co-presidents of the
+            organization. This helped me get an idea of the Campus Y’s target
+            audience, what information the audience is drawn to the most, and
+            what the website is being used for.
+          </p>
+
+          <p>
+            Afterwards, I began conducting usability research of the old website
+            to get a better understanding of what their users are struggling
+            with. This is where I found issues using the main navigation,
+            looking for committees, and completing the room reservation form.
+            You can view the audience analysis and the usability brief below.
+          </p>
 
           {/** Audience Analysis Modal */}
           <div className="btn-center2">
@@ -218,29 +224,121 @@ class CampusY extends Component {
             </Modal.Body>
           </Modal>
 
-          {/** The Design Process */}
-          <h2>Design Process</h2>
-          <h3>Sketches</h3>
+          <h3>User Personas</h3>
           <p>
-            After analyzing the user research on the old website, I began
-            sketchinga design to help me visualize how the new site should
-            look. My first sketches were focused on how the home page of the
-            site should be restructured and how to improve the reservation
-            page by using a calendar. I also came up with an idea to better
-            align the news section on the homepage because the communications
-            director complained about this as a problem in the aesthetics of
-            the site.
+            The personas I created for this project are a representation of the
+            audience that is most likely to use this website. I decided to take
+            an ethnographic approach by studying the target audience in a
+            non-obtrusive way. I sat around the Campus Y observing people’s
+            habits, motives, and discussions with other people around them
+            without getting involved and staying away from the environment.
+            This gave me an understanding of what the audience wants to get out
+            of the Campus Y.
           </p>
 
           <div className="center">
-            <img src={require("../../images/campYSketch.jpg")}
-              alt="First Sketch"
+            <img src={require("../../images/persona1.png")}
+              alt="First Persona"
               className="projectImg2"
             />
 
-            <img src={require("../../images/campYSketch2.jpg")}
-              alt="Second Sketch"
+            <img src={require("../../images/persona2.png")}
+              alt="Second Persona"
               className="projectImg2"
+            />
+          </div>
+
+          <h3 className="topSpac">User Journeys</h3>
+          <p>
+            Once I had my personas defined and understood, I went on to create
+            user journeys to figure out certain touchpoints and painpoints they
+            may encounter when going through the website. This was used to help
+            figure out where I can improve the website and create a better user
+            experience for the Campus Y’s audience.
+          </p>
+
+          <div className="center">
+            <img src={require("../../images/campusY/CampusYUserJourney.png")}
+              alt="First User Journey"
+              className="projectImg2"
+            />
+
+            <img src={require("../../images/campusY/CampusYUserJourney2.png")}
+              alt="Second Persona"
+              className="projectImg2"
+            />
+          </div>
+
+          {/** The Design Process */}
+          <h2 className="topSpac">Design Process</h2>
+          <h3>Sketches for the Committees Page</h3>
+          <p>
+            For the committee’s user flow, I decided to keep the button above
+            the introductory video on the homepage because the communications
+            director mentioned that this was the most important page in the
+            website. This isn’t surprising because during my observations at
+            the Campus Y, I noticed many students talking about committees they
+            wanted to get involved with.
+          </p>
+
+          <p>
+            I also organized the content shown on the committees’ page to make
+            it easier for users to find the committee they are interested in.
+            Compared to the old website layout, this layout reduced the amount
+            of time it took for users to find their committee by a significant
+            amount without any issues.
+          </p>
+
+          <div className="center">
+            <img src={require("../../images/campusY/committees.jpg")}
+              alt="First Sketch"
+              className="projectImg3 topSpac2"
+            />
+          </div>
+
+          <h3 className="topSpac">Sketches for the Room Reservation Page</h3>
+          <p>
+            For the room reservation form on the Campus Y website, I was able to
+            reduce the number of steps that it took to confirm a reservation
+            from seven to four. The old process for making a room reservation
+            caused many users to give up filling out the form and calling to
+            confirm a room. This user flow is meant to ease the frustration from
+            filling out a lengthy form by helping guide users through the
+            process much faster and easier.
+          </p>
+
+          <p>
+            I even created a new calendar user interface to interact with when
+            users try booking a room through the Campus Y. This user interface
+            will give the user a better experience finding a time they can book
+            a room by guiding them step-by-step through the process instead of
+            the user filling out all the information at once in the old
+            website’s user interface.
+          </p>
+
+          <div className="center">
+            <img src={require("../../images/campusY/reservation.jpg")}
+              alt="First Sketch"
+              className="projectImg3 topSpac2"
+            />
+          </div>
+
+          <h3 className="topSpac">Sketches for the Newsletter Page</h3>
+          <p>
+            For the newsletter page, I decided to keep it consistent to the
+            layout as the committees’ page. This gives the user a more natural
+            way of reading each stories title from left to right instead of
+            scrolling down through each story. I even included a user interface
+            where the user can scrape through multiple pages by clicking the
+            next or previous button at the bottom of the page. The user even has
+            the option to choose which page number they would like to go instead
+            of clicking next one page at a time.
+          </p>
+
+          <div className="center">
+            <img src={require("../../images/campusY/newsletter.jpg")}
+              alt="First Sketch"
+              className="projectImg3 topSpac2"
             />
           </div>
 
